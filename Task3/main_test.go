f406483757b7cbd970a99f8149b51cb952fe2a10
Ketan Rathod <ketanrtd1@gmail.com>
2024-02-02 16:06:42 +0530

    added tests to check valid and solvable sudoku

--- Task3/main_test.go
+++ Task3/main_test.go
@@ -4,25 +4,26 @@ import (
 	"testing"
 )
 
-var ValidSudokuTestData [][][]int = [][][]int{
-	{
-		{5, 3, 4, 6, 7, 8, 9, 1, 2},
-		{6, 7, 2, 1, 9, 5, 3, 4, 8},
-		{1, 9, 8, 3, 4, 2, 5, 6, 7},
-		{8, 5, 9, 7, 6, 1, 4, 2, 3},
-		{4, 2, 6, 8, 5, 3, 7, 9, 1},
-		{7, 1, 3, 9, 2, 4, 8, 5, 6},
-		{9, 6, 1, 5, 3, 7, 2, 8, 4},
-		{2, 8, 7, 4, 1, 9, 6, 3, 5},
-		{3, 4, 5, 2, 8, 6, 1, 7, 9},
-	},
+func TestCheckValidSudoku(t *testing.T) {
+
+	for _, s := range Sudokus {
+		got, _, _ := CheckValidSudoku(s.Sudoku)
+		if got == s.IsValid {
+			t.Log("Got it correct")
+		} else {
+			t.Fatal("Want ", s.IsValid, " but got ", got)
+		}
+	}
 }
 
-func TestValidSudoku(t *testing.T) {
-	for _, sudoku := range ValidSudokuTestData {
-		ans := CheckValidSudoku(sudoku)
-		if ans == true {
-			t.Error("Invalid Sudoku")
+func TestCheckSudokuSolvable(t *testing.T) {
+
+	for i, s := range Sudokus {
+		got := CheckSudokuSolvable(&s.Sudoku, 0, 0)
+		if got == s.IsSolvable {
+			t.Log("Got it correct")
+		} else {
+			t.Fatal("Want ", s.IsSolvable, " but got ", got, " for ", i, s)
 		}
 	}
 }
