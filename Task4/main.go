6da9a105dff740e9541fde441582ac96326b0c7e
Ketan Rathod <ketanrtd1@gmail.com>
2024-02-07 09:48:55 +0530

    task4 functionality done

--- Task4/main.go
+++ Task4/main.go
@@ -20,8 +20,8 @@ type Address struct {
 
 // tech.json unmarshal struct
 type Tech struct {
-	Id       int          `json:"id"`
-	TechDets []TechDetail `json:"techDets`
+	Id         int          `json:"id"`
+	TechDetail []TechDetail `json:"techDets"`
 }
 
 type TechDetail struct {
@@ -29,19 +29,60 @@ type TechDetail struct {
 	Exp  float64 `json:"exp"`
 }
 
+// for final output Marshaling
+type TechDetailFinal struct {
+	Tech string  `json:"techdata"`
+	Exp  float64 `json:"exp"`
+}
+
+// contact.json unmarshal struct
+type Contact struct {
+	Id             int            `json:"id"`
+	ContactDetails ContactDetails `json:"contactDets"`
+}
+
+type ContactDetails struct {
+	Email string `json:"email"`
+	Phone Phone  `json:"phone"`
+}
+
+// Final output struct
+type UserInfo struct {
+	UserId      int               `json:"Userid"`
+	Name        string            `json:"Name"`
+	Address     Address           `json:"Address"`
+	TechDetails []TechDetailFinal `json:"TechDetails"`
+	Email       string            `json:"Email"`
+	Phone       Phone             `json:"Phone"`
+}
+
+// Special marshal logic for phone
+
+type Phone string
+
+// func (p Phone) MarshalJSON() ([]byte, error) {
+// 	return json.Marshal(fmt.Sprintf("+91-%v", p))
+// }
+
+// do marshal on userInfo // Data structure to json creation
+var COUNTRY_CODE map[string]string = map[string]string{
+	"IND": "+91",
+	"UK":  "+41",
+}
+
+func (u *UserInfo) MarshalJSON() ([]byte, error) { // With Pointers it will work
+	countryCode := COUNTRY_CODE[u.Address.Country]
+
+	// Pre-processing before marshaling:
+	u.Phone = Phone(fmt.Sprintf("%v-%v", countryCode, u.Phone))
+
+	type UserInfo2 UserInfo              // creating different type
+	return json.Marshal((*UserInfo2)(u)) // Passing as a different type so that it calls default method and not this one only
+
+	// return json.Marshal(newUser) // it will give stack overflow as this looks for custom marshalJson method each time for given type and thus producing error hence I need to create a new type and then marshal it.
+}
+
 func main() {
-	// Get data of all 3 files
-
-	// Then Merge them based on id
-	// Normal case -> all files have equal and all ids
-	// Some files have some id and some have some other ids
-	// Let user.json -> 1 to 3 id and tech.json this id and so on
-
-	// Get all the id's
-	// Data structure for storing id and relevent other information for particular file
-	// Then can easily iterate over all id's and gather data
-
-	// Lets start
 
 	userFile, err := os.ReadFile("user.json")
 
@@ -49,27 +90,86 @@ func main() {
 		panic(err)
 	}
 
-	var user []User
-	err = json.Unmarshal([]byte(userFile), &user)
+	var users []User
+	err = json.Unmarshal([]byte(userFile), &users)
 	if err != nil {
 		panic(err)
 	}
 	// got data in user
-	fmt.Println(user)
+	// fmt.Println(users)
 
-	// Same for other 2 files
+	// Same for Tech
 	techFile, err := os.ReadFile("tech.json")
 
 	if err != nil {
 		panic(err)
 	}
 
-	var tech []Tech
-	err = json.Unmarshal([]byte(techFile), &tech)
+	var techs []Tech
+	err = json.Unmarshal([]byte(techFile), &techs)
 	if err != nil {
 		panic(err)
 	}
 	// got data in user
-	fmt.Println(tech)
+	fmt.Println(techs)
 
+	// Same for contact.json
+	contactFile, err := os.ReadFile("contact.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	var contacts []Contact
+	err = json.Unmarshal([]byte(contactFile), &contacts)
+	if err != nil {
+		panic(err)
+	}
+	// got data in user
+	// fmt.Println(contacts)
+
+	// Now merge all
+	// Simple Join of users, contacts, and techs
+
+	var userInfos []UserInfo
+	for _, user := range users {
+		for _, tech := range techs {
+			for _, contact := range contacts {
+				if user.Id == tech.Id && user.Id == contact.Id { // if tino referencing to same one then merge
+
+					// modify tech details
+					// Transform TechDetail into TechDetailFinal
+					fmt.Println("Tech details", tech.TechDetail)
+					var techFinalDetails []TechDetailFinal
+					for _, t := range tech.TechDetail {
+						fmt.Println(t)
+						techFinalDetails = append(techFinalDetails, TechDetailFinal{t.Tech, t.Exp})
+					}
+
+					newUserInfo := UserInfo{UserId: user.Id, Name: user.Name, Address: user.Address, TechDetails: techFinalDetails, Email: contact.ContactDetails.Email, Phone: contact.ContactDetails.Phone}
+					userInfos = append(userInfos, newUserInfo)
+				}
+			}
+		}
+	}
+
+	fmt.Println(userInfos)
+
+	userInfoBytes, err := json.Marshal(userInfos)
+	if err != nil {
+		panic(err)
+	}
+
+	WriteJSONToFile(userInfoBytes)
+
+	// Alternative 2
+	// can we do it using map // store information got in map and update it
+
+}
+
+func WriteJSONToFile(jsonBytes []byte) error {
+	err := os.WriteFile("result.json", jsonBytes, os.FileMode(0644))
+	fmt.Println("\nFinal Output is in result.json\n")
+
+	return err
 }
