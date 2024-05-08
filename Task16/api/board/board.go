6600842d5db697e8d3336fb7950f7c6217bcadce
Ketan Rathod <ketanrtd1@gmail.com>
2024-05-08 16:26:45 +0530

    added find by title resolver

--- Task16/api/board/board.go
+++ Task16/api/board/board.go
@@ -1,9 +1,40 @@
 package board
 
-import "graphql_search/models"
+import (
+	"context"
+	"graphql_search/models"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func (a *api) GetBoardsByTitle(title string) ([]*models.Board, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-	
-	return nil, nil
+	var boardsDb []*models.BoardDB
+
+	cursor, err := a.Database.Collection("products").Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = cursor.All(ctx, &boardsDb)
+	if err != nil {
+		return nil, err
+	}
+
+	var boards []*models.Board
+	for _, p := range boardsDb {
+		boards = append(boards, &models.Board{
+			ID:          p.ID.Hex(),
+			BoardID:     p.BoardID,
+			Visible:     p.Visible,
+			Description: p.Description,
+			Title:       p.Title,
+			Type:        p.Type,
+		})
+	}
+
+	return boards, nil
 }
--- Task16/models/Board.go
+++ Task16/models/Board.go
@@ -1,5 +1,7 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Board struct {
 	ID          string `json:"Id"`
 	BoardID     string `json:"boardId"`
@@ -7,4 +9,13 @@ type Board struct {
 	Description string `json:"description"`
 	Title       string `json:"title"`
 	Type        string `json:"type"`
+}
+
+type BoardDB struct {
+	ID          primitive.ObjectID `json:"Id" bson:"_id"`
+	BoardID     string             `json:"boardId"`
+	Visible     string             `json:"visible"`
+	Description string             `json:"description"`
+	Title       string             `json:"title"`
+	Type        string             `json:"type"`
 }
